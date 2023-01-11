5d8eec59334e35abf2dc0b8499c30ca04311e794
y2gon2 <y2gon2@gmail.com>
2023-01-11 09:45:48 +0900

    gpio

--- tutorial/src/bin/led.rs
+++ tutorial/src/bin/led.rs
@@ -1,9 +1,23 @@
-use rust_gpiozero::LED;
+// use rust_gpiozero::LED;
+
+// fn main() {
+//     loop {
+//         let mut led = LED::new(17);
+
+//         led.blink(2.0, 3.0);
+//     }
+// }
+
+use gpio::{GpioIn, GpioOut};
+use std::{thread, time};
 
 fn main() {
-    loop {
-        let mut led = LED::new(17);
+    let mut gpio17 = gpio::sysfs::SysFsGpioOutput::open(24).unwrap();
+    let mut signal = false;
 
-        led.blink(2.0, 3.0);
+    loop {
+        gpio17.set_value(signal).expect("could not set");
+        thread::sleep(time::Duration::from_secs(1));
+        signal = !signal;        
     }
 }
