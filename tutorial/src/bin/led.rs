e2e61f0624872c3df98cdb053f3c4b4467104416
y2gon2 <y2gon2@gmail.com>
2023-01-11 10:03:53 +0900

    adf

--- tutorial/src/bin/led.rs
+++ tutorial/src/bin/led.rs
@@ -8,16 +8,34 @@
 //     }
 // }
 
-use gpio::{GpioIn, GpioOut};
-use std::{thread, time};
+// use gpio::{GpioIn, GpioOut};
+// use std::{thread, time};
 
-fn main() {
-    let mut gpio17 = gpio::sysfs::SysFsGpioOutput::open(17).unwrap();
-    let mut signal = false;
+// fn main() {
+//     let mut gpio17 = gpio::sysfs::SysFsGpioOutput::open(17).unwrap();
+//     let mut signal = false;
+
+//     loop {
+//         gpio17.set_value(signal).expect("could not set");
+//         thread::sleep(time::Duration::from_secs(1));
+//         signal = !signal;        
+//     }
+// }
+
+use c2_mmap_gpio::{Device, PinId, Value};
+use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut odroid = Device::new()?;
+    let mut led_pin = odroid.output_pin(PinId::Phy7)?;
+    let blink_interval = Duration::from_millis(500);
 
     loop {
-        gpio17.set_value(signal).expect("could not set");
-        thread::sleep(time::Duration::from_secs(1));
-        signal = !signal;        
+        led_pin.set_value(Value::High);
+        sleep(blink_interval);
+        led_pin.set_value(Value::Low);
+        sleep(blink_interval);
     }
 }
